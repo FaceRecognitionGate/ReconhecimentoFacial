e89ce5bc6b449c4e4e4d140f5ca1e065b190588c
marcelogdeandrade <marcelogdeandrade@github.com>
2016-10-28 08:55:28 -0200

    started main class

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	private void runDocker(){
+		
+		String command = "docker run -p 9000:9000 -p 8000:8000 -t -i bamos/openface /bin/bash";
+		
+		String output = this.executeCommand(command);
+		
+		System.out.println(output);
+	}
+	
+	private void trainDatabase(){
+		
+		this.runDocker();
+		
+		String command = "cd /root/openface && for N in {1..8}; do ./util/align-dlib.py <path-to-raw-data> "
+				+ "align outerEyesAndNose <path-to-aligned-data> --size 96 & done && ./batch-represent/main.lua "
+				+ "-outDir <feature-directory> -data <path-to-aligned-data> && ./demos/classifier.py train <feature-directory>";
+		
+		String output = this.executeCommand(command);
+		
+		System.out.println(output);
+	}
+	
+	private String executeCommand(String command) {
+		
+		StringBuffer output = new StringBuffer();
+
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+                        String line = "";
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return output.toString();
+
+	}
+}
