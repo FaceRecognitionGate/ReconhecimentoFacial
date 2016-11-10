4e291c732ccf7307c49cb0e84f73e372b23fd613
Pedro Cunial <pedrocunial@gmail.com>
2016-11-10 15:09:34 -0200

    Comecei a criar a classe de utils do zip

---
+++ src/br/edu/insper/recfacial/utils/Zip.java
@@ -0,0 +1,23 @@
+package br.edu.insper.recfacial.utils;
+
+
+public class Zip {
+//    String source = "some/compressed/file.zip";
+//    String destination = "some/destination/folder";
+//    String password = "password";
+//
+//    try {
+//         ZipFile zipFile = new ZipFile(source);
+//         if (zipFile.isEncrypted()) {
+//            zipFile.setPassword(password);
+//         }
+//         zipFile.extractAll(destination);
+//    } catch (ZipException e) {
+//        e.printStackTrace();
+//    }	
+	public void unZipStream(String zipFile) {
+		// Unzips files from given zipinputstream
+		String fullPath = "/opt/data1/zip" + zipFile + ".zip";
+	}
+
+}
