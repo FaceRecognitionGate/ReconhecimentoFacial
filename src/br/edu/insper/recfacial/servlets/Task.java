b69d84d2aa48475ff4271f2a5ec5f07e546607d0
Pedro Cunial <pedrocunial@gmail.com>
2016-11-17 20:28:19 -0200

    esperando

---
+++ src/br/edu/insper/recfacial/servlets/Task.java
@@ -0,0 +1,26 @@
+package br.edu.insper.recfacial.servlets;
+
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
+public class Task extends Thread implements HttpSessionBindingListener {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void valueBound(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void valueUnbound(HttpSessionBindingEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/br/edu/insper/recfacial/servlets/WebCam.java
@@ -0,0 +1,28 @@
+package br.edu.insper.recfacial.servlets;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class WebCam implements ServletContextListener {
+
+	private ExecutorService executor;
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		executor = Executors.newSingleThreadExecutor();
+		executor.submit(new Task);
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		executor.shutdown();
+	}
+	
+	
+}
