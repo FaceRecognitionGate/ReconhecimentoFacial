7063b1f681e1b66f40dafee62b66a448119977f5
marcelogdeandrade <marcelogdeandrade@github.com>
2016-11-17 13:47:05 -0200

    changed docker class and processemail servlet

--- src/br/edu/insper/recfacial/utils/Docker.java
+++ src/br/edu/insper/recfacial/utils/Docker.java
@@ -10,36 +10,36 @@ public final class Docker {
 	}
 
 
-	private String executeCommand(String command) throws DockerNotConnectedException {
+	public static String executeCommand(String command){
 		// Executa um dado comando na CLI do docker
-			StringBuffer output = new StringBuffer();
-			Process p;
-			try {
-				p = Runtime.getRuntime().exec(command);
-				p.waitFor();
-				BufferedReader reader =
-	                            new BufferedReader(new InputStreamReader(p.getInputStream()));
-                String line = "";
-				while ((line = reader.readLine())!= null) {
-					output.append(line + "\n");
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
+		StringBuffer output = new StringBuffer();
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader =
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line = "";
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
 			}
-			return output.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return output.toString();
 	}
 
 
-	public void trainDatabase() throws DockerNotConnectedException {
+	public static void trainDatabase(){
 		// Treina a base de dados à partir das imagens já salvas no docker
 		String command = Constants.TRAIN_DATA;
 		
-		String output = this.executeCommand(command);
+		String output = Docker.executeCommand(command);
 
 		System.out.println(output);
 	}
 
-	public void testImage(String path) throws DockerNotConnectedException{
+	public static void testImage(String path){
 		// Tests if a image at a given path corresponds to a known person
 		String command = Constants.TEST_DATA + " " + path + " && exit";
 
@@ -48,12 +48,11 @@ public final class Docker {
 		System.out.println(output);
 	}
 	
-	public int mkdir(String dirName) {
+	public static int mkdir(String dirName){
 		// Returns 0 if successful, 1 if not
 			String command = "mkdir " + Constants.RAW_PICTURES_DIR_NODOCKER + "/" + dirName;
 			String output = executeCommand(command);
 			return 0;
-		return 1;
 	}
 
 
